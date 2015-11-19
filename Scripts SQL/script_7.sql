d117f2bac32b57c479642a9f7dbff1b6465ba7dd
danpatrocinio <dpatrocinio@gmail.com>
2015-11-18 23:23:09 -0200

    Ajuste para senha admin do usuário administrador

--- Scripts SQL/script_7.sql
+++ Scripts SQL/script_7.sql
@@ -2,4 +2,4 @@ ALTER TABLE usuarios CHANGE cod_usuarios cod_usuarios INT(15) NOT NULL AUTO_INCR
 ALTER TABLE usuarios CHANGE COLUMN senha senha VARCHAR(200) NOT NULL;
 ALTER TABLE usuarios ADD COLUMN login VARCHAR(80) NOT NULL AFTER cod_usuarios;
 
-INSERT INTO usuarios (cod_usuarios, login, senha, nome, tipo) VALUES (NULL, 'administrador', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'Administrador', 'a');
+INSERT INTO usuarios (cod_usuarios, login, senha, nome, tipo) VALUES (NULL, 'administrador', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'Administrador', 'a');
